cde40261112504694f3b0feb29c7988ea0641527
Alan Mooiman <amooiman@gmail.com>
2024-03-08 14:29:47 -0500

    Switch to Supabase database

--- fourstoryFlask/__init__.py
+++ fourstoryFlask/__init__.py
@@ -10,7 +10,10 @@ def create_app():
     app = Flask(__name__)
     # Fix for when the app is behind GCP proxy, as the auth request url for foursquare ends up being http otherwise
     app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fourstory.db'
+    DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+    DATABASE_USER = os.getenv('DATABASE_USER')
+    DATABASE_HOST = os.getenv('DATABASE_HOST')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}"
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     db.init_app(app)
 
